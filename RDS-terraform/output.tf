1e13829d23765ab60bc4972ac09bd5e90dcb6d4d
Ross Simpson <thisisrosssimpson@gmail.com>
2024-03-27 17:04:44 +0100

    make code review changes to route table associations

--- RDS-terraform/output.tf
+++ RDS-terraform/output.tf
@@ -1,10 +1,4 @@
-# output "rds_instance_endpoint" {
-#   value = aws_db_instance.mydb.endpoint
-# }
 
-# output "rds_security_group_id" {
-#   value = aws_security_group.my_sg.id
-# }
 
 output "rds_instance_endpoint" {
   description = "The connection endpoint for the RDS instance."
