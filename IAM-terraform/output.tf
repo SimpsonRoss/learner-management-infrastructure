f7206a58a2a4972ca55ec4e0e611a8ba3d7de8f9
Ross Simpson <thisisrosssimpson@gmail.com>
2024-03-27 11:08:56 +0100

    Remove IAM-terraform dir, and refactor RDS into modules and secure variables

--- IAM-terraform/output.tf
+++
@@ -1,3 +0,0 @@
-output "team_role_arn" {
-  value = aws_iam_role.team_role.arn
-}
---
+++ RDS-terraform/modules/security/main.tf
@@ -0,0 +1,16 @@
+resource "aws_security_group" "my_sg" {
+  name        = var.sg_name
+  description = "Allow PostgreSQL"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# output "sg_id" {
+#   value = aws_security_group.my_sg.id
+# }
